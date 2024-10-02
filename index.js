780ceb066f743a06fcd18841e573e0aecda0773b
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-02 19:55:36 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftPosition = parseInt(dodger.style.left) || 0;
+    if (leftPosition > 0) {
+        dodger.style.left = `${leftPosition - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftPosition = parseInt(dodger.style.left) || 0;
+    dodger.style.left = `${leftPosition + 1}px`;
+}
